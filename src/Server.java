1a9b29a571bdb2c68ac8bc7c79e8c4b7478f1352
ahak14 <ah.abbasi.ah@gmail.com>
2019-07-03 16:44:14 +0430

    complete chatroom

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,20 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        ServerSocket serverSocket = new ServerSocket(8888);
+
+        while(true) {
+
+            System.err.println("Waiting for connect a client ...");
+
+            ClientThread clientThread = new ClientThread(serverSocket.accept());
+            clientThread.start();
+
+            System.err.println("Client connected");
+        }
     }
 }
--- src/packet/LoginPacket.java
+++ src/packet/LoginPacket.java
@@ -1,4 +1,12 @@
 package packet;
 
 public class LoginPacket extends Packet {
+
+    String userName, password;
+
+    public LoginPacket(String userName, String password) {
+
+        this.userName = userName;
+        this.password = password;
+    }
 }
